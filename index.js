34fdb7ea32666f3321ac66c2a489521efcf48767
Ryan <ryan@thecpafirm.com>
2022-05-28 22:22:26 -0600

    original method

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 const express = require('express')
 const request = require('request')
 const bodyParser = require('body-parser')
-const cors = require('cors')
+// const cors = require('cors')
 const app = express()
 
 let myLimit = typeof(process.argv[2]) != 'undefined' ? process.argv[2] : '100kb';
 console.log('Using limit: ', myLimit);
 
 app.use(bodyParser.json({limit: myLimit}));
-app.use(cors({
-    origin: '*',
-    methods: ['GET', 'PUT', 'PATCH',  'POST', 'DELETE'],
-    credentials: true,
-}))
+// app.use(cors({
+//     origin: '*',
+//     methods: ['GET', 'PUT', 'PATCH',  'POST', 'DELETE'],
+//     credentials: true,
+// }))
 
 app.all('*', function (req, res, next) {
 
     // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
-    // res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
-    // res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
-    // res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
+    res.header("Access-Control-Allow-Origin", "*");
 
     if (req.method === 'OPTIONS') {
         // CORS Preflight
@@ -40,7 +40,7 @@ app.all('*', function (req, res, next) {
     }
 });
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3005);
 
 app.listen(app.get('port'), function () {
     console.log('Proxy server listening on port ' + app.get('port'));
