454eb4de1103f685ce2b7158c469190bdc5e1a1a
Ryan <ryan@thecpafirm.com>
2022-05-30 20:21:34 -0600

    update to fetch path

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ app.use(bodyParser.json({limit: myLimit}));
 app.all('*', function (req, res, next) {
 
     // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
-    // res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
-    // res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
-    // res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
+    res.header("Access-Control-Allow-Origin", "*");
 
     if (req.method === 'OPTIONS') {
         // CORS Preflight
@@ -30,17 +30,12 @@ app.all('*', function (req, res, next) {
             res.send(500, { error: 'There is no Target-Endpoint header in the request' });
             return;
         }
-        request({ url: targetURL + req.url, method: req.method, json: req.body, headers: {'Authorization': req.header('Authorization')} },
+        request({ url: targetURL + req.url + '/', method: req.method, json: req.body, headers: {'Authorization': req.header('Authorization')} },
             function (error, response, body) {
                 if (error) {
                     console.error('error: ' + response.statusCode)
                 }
 //                console.log(body);
-
-                res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
-                res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
-                res.header("Access-Control-Allow-Origin", "*");
-                
             }).pipe(res);
     }
 });
