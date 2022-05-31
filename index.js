0d129a32fd053d8afd40a00989d47fbf9bab2013
Ryan <ryan@thecpafirm.com>
2022-05-31 12:52:29 -0600

    update to res credentials

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.all('*', function (req, res, next) {
     res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
     res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
     res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Credentials', 'true')
 
     if (req.method === 'OPTIONS') {
         // CORS Preflight
